0b3fcdb859c8d69e0bc0c555f717a9f3c4e9941e
Fábio-Developer <developer.fabio.jr@gmail.com>
2024-04-05 20:08:23 -0300

    first commit

---
+++ teste.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int idade;
+    float altura;
+
+    cin >> idade;
+    cin >> altura;
+
+    cout << "A idade e: " << idade << endl;
+    cout << "A altura e: " << altura << endl;
+
+
+    return 0;
+}
