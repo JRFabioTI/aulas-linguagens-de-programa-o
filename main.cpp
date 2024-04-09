cb1ef5e585f96a61e91cc9e982218d4d94a58fc3
Fábio Jr <tenfrancapm@gmail.com>
2024-04-08 22:21:57 -0300

    Add files via upload
    Exercício em c++ aula 08/04

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    string nome;
+    string sobrenome;
+
+    cin >> nome;
+    cin >> sobrenome;
+
+    int ultima_posicao = nome.size() - 1;
+    cout << sobrenome << nome[0] << nome[ultima_posicao] << endl;
+
+    return 0;
+}
